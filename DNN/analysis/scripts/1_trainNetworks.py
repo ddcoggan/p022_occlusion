94d6224b287bc95dcdeb0fc40643c6a38e0481b8
ddcoggan <ddcoggan@gmail.com>
2022-09-01 11:07:16 -0500

    fixed accidental batch-wise scheduler steps rather than epoch-wise

--- DNN/analysis/scripts/1_trainNetworks.py
+++ DNN/analysis/scripts/1_trainNetworks.py
@@ -21,7 +21,7 @@ config = {'modelParams': {'pretrained': False},
                              'skipZeroth': False,
                              'workers': 8,
                              'nGPUs': 1,
-                             'GPUids': 0}}
+                             'GPUids': 1}}
 
 
 # list various occluder types and levels
@@ -70,8 +70,8 @@ analyses = {'allAlexnet': {'alexnet': {'imagenet16': {'occluders': occluders, 'c
 #analyses = {'test': {'alexnet': {'imagenet16': {'occluders': ['unoccluded']}}}}
 
 analyses = {#'cornet_s_varRec_1_1_1_1': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [1,1,1,1]}}}}
-            'cornet_s_varRec_2_2_4_2': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [2,2,4,2]}}}}
-            #'cornet_s_varRec_5_5_10_5': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [5,5,10,5]}}}}
+            #'cornet_s_varRec_2_2_4_2': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [2,2,4,2]}}}}
+            'cornet_s_varRec_5_5_10_5': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [5,5,10,5]}}}}
             #'cornet_s_varRec_1_1_1_1': {'cornet_s_varRec': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': [1,1,1,1]}}}}
             #'cornet_s_varRec_2_2_4_2': {'cornet_s_varRec': {'imagenet16': {'occluders': ['unoccluded'], 'coverages': [.5], 'times': [2,2,4,2]}}}}
             #'cornet_s_varRec_varRF_2_2_4_2-3': {'cornet_s_varRec': {'imagenet16': {'occluders': ['unoccluded'], 'coverages': [.5], 'times': [2,2,4,2], 'RF': 3}}}}
