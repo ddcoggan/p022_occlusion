f627e62815e9f3a01e57bae8c4137cdea734f2e4
ddcoggan <ddcoggan@gmail.com>
2022-09-21 16:36:18 -0500

    Wed 21 Sep 2022 04:36:09 PM CDT

--- DNN/analysis/scripts/1_trainNetworks.py
+++ DNN/analysis/scripts/1_trainNetworks.py
@@ -2,193 +2,129 @@ import os
 import glob
 import sys
 import datetime
-
-sys.path.append(f'{os.path.expanduser("~")}/Dave/masterScripts/DNN')
-from train import train
 import time
 #time.sleep(18000)
 
-overwrite = False
+sys.path.append(f'{os.path.expanduser("~")}/Dave/masterScripts/DNN')
+from train import train
 
-# set default / general parameters
-printTheseParams = ['modelName','times','RF','occluder']
-config = {'modelParams': {'pretrained': False},
-          'datasetParams': {'propOccluded': .8,
-                            'colours': [(0,0,0),(127,127,127),(255,255,255)],
-                            'invert': False},
-          'trainingParams': {'learningRate': .01,
-                             'optimizerName': 'SGD',
-                             'nEpochs': 32,
-                             'workers': 8,
-                             'nGPUs': 1,
-                             'GPUids': 1}}
+### CONFIGURATION ###
 
-#analyses = {'test': {'alexnet': {'imagenet1000': {'occluders': ['unoccluded']}}}}
-analyses = {'cornet_s_custom_Rec1-1-1-1_RF3-3-3-3': {'cornet_s_custom': {'imagenet1000': {'occluders': ['unoccluded'], 'coverages': [.5], 'times': (1,1,1,1), 'RF': (3,3,3,3)}}}}
-            #'cornet_s_custom_Rec1-2-4-2_RF3-3-3-3': {'cornet_s_custom': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': (1,2,4,2), 'RF': (3,3,3,3)}}}}
-            #'cornet_s_custom_Rec2-4-8-4_RF3-3-3-3': {'cornet_s_custom': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': (2,4,8,4), 'RF': (3,3,3,3)}}}}
-            #'cornet_s_custom_Rec1-2-4-2_RF5-5-5-5': {'cornet_s_custom': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': (1,2,4,2), 'RF': (5,5,5,5)}}}}
-            #'cornet_s_custom_Rec1-2-4-2_RF7-7-7-7': {'cornet_s_custom': {'imagenet1000': {'occluders': ['barHorz08', 'unoccluded'], 'coverages': [.5], 'times': (1,2,4,2), 'RF': (7,7,7,7)}}}}
-            #'cornet_s_custom_predify_Rec1-2-4-2_RF3-3-3-3': {'cornet_s_custom_predify': {'imagenet1000': {'occluders': ['barHorz08'], 'coverages': [.5], 'times': (1,2,4,2), 'RF': (3,3,3,3)}}}}
-
-# list various occluder types and levels
-occluders = []
-for x in sorted(glob.glob('DNN/images/occluders/*')):
-    occluders.append(os.path.basename(x))
-occluders.append('unoccluded')
-
+# often used occluders and visibility levels
 occludersFMRI = ['barHorz04','barVert12','barHorz08']
-occludersBehavioural = ['barHorz04', 'barVert04', 'barObl04', 'mudSplash', 'polkadot','polkasquare','crossBarOblique','crossBarCardinal', 'naturalUntexturedCropped2']
-occludersNoLevels = ['naturalTextured','naturalTextured2']
-occludersWithLevels = occluders
-for o in occludersNoLevels:
-    occludersWithLevels.remove(o)
-occludersWithLevels.remove('unoccluded')
+occludersBehavioural = ['barHorz04','barVert04','barObl04','mudSplash','polkadot','polkasquare','crossBarOblique','crossBarCardinal', 'naturalUntexturedCropped2']
+occludersNoLevels = ['naturalTextured','naturalTextured1','naturalTextured2']
+visibilities = [.1,.2,.4,.8]
 
-# standard coverage levels
-indCoverages = [.1,.2,.4,.8]
+# model (modelName*, model, R, K, pretrained, outDir, lastEpoch)
+m = {
+    'modelName': 'cornet_s_custom',
+    'R': (1,2,4,2), # recurrence (cornet_s_custom)
+    'K': (3,3,3,3), # kernel size (cornet_s_custom)
+    }
 
-batchSizes = {'alexnet': 1024,
-              'vgg19': 128,
-              'cornet_s': 256,
-              'cornet_s_custom': 64,
-              'cornet_s_custom_predify': 128,
-              'resnet18': 512,
-              'resnet34': 256,
-              'resnet50': 64,
-              'resnet101': 64,
-              'resnet152': 64,
-              'inception_v3': 32,
-              'PredNetImageNet': 8}
+# dataset (path*,occlusion{type,label,propOccluded,visibility,colour,label},
+#          blur{sigmas,weights,label}, sigmas: [0, 1, 2, 4, 8]
+#          noise{type,ssnr,label}) ssnr: [.1,.2,.4,.8,1]
+d = {
+    'dataset': 'ILSVRC2012',
+    'occlusion': {'type': occludersBehavioural,'label': 'behaviouralOccs',
+                  'propOccluded': .8,'visibility': visibilities, 'visLabel': 'mixedVis',
+                  'colour': [(0,0,0),(127,127,127),(255,255,255)]}
+    }
 
-for analysis in analyses:
+# training (optimizerName*, nEpochs*, nGPUs, GPUids, learningRate, batchSize, workers)
+t = {
+    'optimizerName': 'SGD',
+    'nEpochs': 32,
+    'nGPUs': 1,  # set to -1 to use all available GPUs, 0 to use CPU
+    'GPUids': 0,  # ignored if nGPUs in [-1,0]
+    'learningRate': 2**-7,
+    'batchSize': 2**5
+    }
 
-    for modelName in analyses[analysis]:
+printTheseParams = ['modelName','R','K','datasetParams.occlusion.label']
 
-        config['modelParams']['modelName'] = modelName
+### END OF CONFIGURATION ###
 
-        # force 1 gpu for certain networks
-        if modelName.startswith('cornet') or modelName in ['PredNetImageNet']:
-            config['trainingParams']['nGPUs']=1
 
-        # force pretrained for certain networks
-        if modelName.startswith('cornet_s_custom'):
-            config['modelParams']['pretrained'] = False
-        elif modelName in 'PredNetImageNet':
-            config['modelParams']['pretrained'] = False
+# set pretrained to False by default and enforce no pretraining for some models
+if not 'pretrained' in m:
+    m['pretrained'] = False
+elif m['pretrained'] and m['modelName'] in ['cornet_s_custom', 'cornet_s_custom_predify']:
+    UserWarning(f"Pretrained weights for {m['modelName']} are not available, training from random initialized weights instead.")
+    m['pretrained'] = False
 
-        # set pretrained string
-        if config['modelParams']['pretrained']:
-            pretrainedString = 'fromPretrained'
+# set model training directory
+if 'outDir' not in m:
+    pretrainedString = ['fromScratch', 'fromPretrained'][m['pretrained']]
+    pathItems = [m['modelName']]
+    if 'custom' in m['modelName']:
+        pathItems += [f"recCycles_{m['R'][0]}-{m['R'][1]}-{m['R'][2]}-{m['R'][3]}"]
+        pathItems += [f"kernelSizes_{m['K'][0]}-{m['K'][1]}-{m['K'][2]}-{m['K'][3]}"]
+    pathItems += [d['dataset'], pretrainedString]
+
+    # image distortions
+    imageDist = 'unaltered'
+    if 'occlusion' in d:
+        try: imageDist = d['occlusion']['label']
+        except: imageDist = d['occlusion']['type']
+        try: imageDist += f"_vis-{d['occlusion']['visLabel']}"
+        except: imageDist += f"_vis-{int(d['occlusion']['visibility']*100):02}"
+    if 'blur' in d:
+        imageDist = f"__{d['blur']['label']}"
+    if 'noise' in d:
+        imageDist += f"__{d['noise']['label']}"
+    pathItems += [imageDist]
+
+    m['outDir'] = 'DNN/data'
+    for item in pathItems:
+        m['outDir'] = os.path.join(m['outDir'], item)
+
+# get most recent params file if possible
+if 'lastEpoch' not in m:
+    paramsPaths = sorted(glob.glob(os.path.join(m['outDir'], 'params/*.pt')))
+    if paramsPaths:
+        m['lastEpoch'] = int(os.path.basename(paramsPaths[-1])[:-3])
+    else:
+        m['lastEpoch'] = None
+
+# call script
+if m['lastEpoch'] is None or m['lastEpoch'] < t['nEpochs']:
+
+    # enforce single GPUs for some models
+    if t['nGPUs'] not in [0, 1] and m['modelName'] in ['cornet_s_custom', 'cornet_s_custom_predify', 'PredNetImageNet']:
+        print(f"{m['modelName']} cannot be trained on multiple GPUs, training on 1 GPU instead.")
+        t['nGPUs'] = 1
+
+    # set GPU visibility (do BEFORE anything using cuda (e.g. torch) is imported)
+    if t['nGPUs'] == 1:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = f"{t['GPUids']}"
+
+    # remove warnings for predified networks
+    if m['modelName'].endswith('predify'):
+        import warnings
+        warnings.simplefilter("ignore")
+
+    # make training configuration dictionary
+    config = {'modelParams': m, 'datasetParams': d, 'trainingParams': t}
+
+    # print out training configuration information
+    printString = f'Started at {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+    for param in printTheseParams:
+        if '.' not in param:
+            for thisDict in config:
+                if param in config[thisDict]:
+                    printString += f' | {param}: {config[thisDict][param]}'
         else:
-            pretrainedString = 'fromScratch'
+            thisDict, thisSubdict, thisParam = param.split('.')
+            if thisDict in config and thisParam in config[thisDict]:
+                printString += f' | {thisParam}: {config[thisDict][thisParam]}'
+    print(printString)
 
-        # get batch size
-        config['trainingParams']['batchSize'] = batchSizes[modelName]
-
-        # remove warnings for predified networks
-        if modelName.endswith('predify'):
-            import warnings
-            warnings.simplefilter("ignore")
-
-        for dataset in analyses[analysis][modelName]:
-
-            config['datasetParams']['datasetPath'] = f'{os.path.expanduser("~")}/Datasets/{dataset}'
-            config['datasetParams']['datasetName'] = dataset
-
-            modelNameFull = modelName
-            if modelName.startswith('cornet_s_'):
-                config['modelParams']['times'] = analyses[analysis][modelName][dataset]['times']
-                modelNameFull = analysis
-                config['modelParams']['RF'] = analyses[analysis][modelName][dataset]['RF']
-
-            for occluder in analyses[analysis][modelName][dataset]['occluders']:
-
-                config['datasetParams']['occluder'] = occluder
-                if type(occluder) is list:
-                    occluderString = 'mixedOccluders' # if different occluder types are used simultaneously
-                else:
-                    occluderString = occluder
-
-                if occluder == 'unoccluded':
-                    coverages = [None]
-                    coverageString = ['no coverage']
-
-                elif occluder in occludersNoLevels:
-                    coverages = [None]
-                    coverageString = ['all occluders']
-                else:
-                    coverages = analyses[analysis][modelName][dataset]['coverages']
-
-                outDir = os.path.join('DNN/data', modelNameFull, dataset, pretrainedString, occluder)
-
-                for coverage in coverages:
-
-                    # set up out dir based on occlusion, blur, noise
-
-                    # occlusion
-                    if type(coverage) is list:
-                        coverageString = 'mixedLevels' # if different occluder levels are used simultaneously
-                    elif coverage:
-                        coverageString = str(int(coverage*100))
-
-                    if coverage:
-                        outDir = os.path.join(outDir, coverageString)
-                        config['datasetParams']['coverage'] = coverage
-
-                    # blur
-                    if 'mixedBlur' in analysis:
-                        outDir += '_mixedBlur'
-                        config['datasetParams']['blur'] = True
-                        config['datasetParams']['blurSigmas'] = [0, 1, 2, 4, 8],
-                        config['datasetParams']['blurWeights'] = [.2, .2, .2, .2, .2]
-                    else:
-                        config['datasetParams']['blur'] = False
-
-                    # noise
-                    if 'mixedNoise' in analysis:
-                        outDir += '_mixedNoise'
-                        config['datasetParams']['noise'] = True
-                        config['datasetParams']['noiseLevels'] = [1,.8,.4,.2,.1]
-                    else:
-                        config['datasetParams']['noise'] = False
-
-                    config['modelParams']['outDir'] = outDir
-
-                    if os.path.exists(f'{outDir}/plotStats.pkl'):
-                        import shutil
-                        shutil.copy(f'{outDir}/plotStats.pkl', f'{outDir}/plots/plotStats.pkl')
-
-                    # get most recent params file if possible
-                    paramsPaths = sorted(glob.glob(os.path.join(outDir, 'params/*.pt')))
-                    if paramsPaths and not overwrite:
-                        config['modelParams']['lastEpoch'] = int(os.path.basename(paramsPaths[-1])[:-3])
-                    else:
-                        config['modelParams']['lastEpoch'] = None
-
-                    # call script
-                    if len(paramsPaths) < config['trainingParams']['nEpochs']+1 or overwrite:
-
-                        # set GPUs
-                        if config['trainingParams']['nGPUs'] == 1:
-                            os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-                            os.environ["CUDA_VISIBLE_DEVICES"] = f"{config['trainingParams']['GPUids']}"
-
-                        # create config dictionary
-                        printOut = {}
-                        for param in printTheseParams:
-                            for set in config:
-                                if param in config[set]:
-                                    printOut[param] = config[set][param]
-
-                        # print out config information
-                        printString = f'Started at {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
-                        for p in printOut:
-                            printString += f' | {p}: {printOut[p]}'
-                        print(printString)
-
-                        # train
-                        train(**config)
+    # train
+    train(**config)
 
 '''
 analyses = {'allAlexnet': {'alexnet': {'imagenet16': {'occluders': occluders, 'coverages': indCoverages}}},
